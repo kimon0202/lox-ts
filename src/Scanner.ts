446593348f0f238c24321fb5bcbc232757298897
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2020-11-24 21:11:22 -0300

    Working scanner

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,6 +1,7 @@
 import { TokenType } from './TokenType';
 import { Token } from './Token';
 import LoxEvents from './emitter/Lox';
+import { keywords } from './contants/ReservedKeywords';
 
 export class Scanner {
   private source: string;
@@ -38,6 +39,76 @@ export class Scanner {
     this.tokens.push(new Token(type, text, literal, this.line));
   }
 
+  private match(expected: string): boolean {
+    if (this.isAtEnd()) return false;
+    if (this.source[this.current] !== expected) return false;
+
+    this.current++;
+    return true;
+  }
+
+  private peek(offset = 0): string {
+    if (this.isAtEnd() || this.current + offset > this.source.length)
+      return '\0';
+    return this.source[this.current + offset];
+  }
+
+  private string(): void {
+    while (this.peek() !== '"' && !this.isAtEnd()) {
+      if (this.peek() === '\n') this.line++;
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      LoxEvents.emit('error', 'Unterminated string.');
+      return;
+    }
+
+    // closing "
+    this.advance();
+
+    // get the text
+    // remove the quotes from the string value
+    const value = this.source.substring(this.start + 1, this.current - 1);
+    this.addToken(TokenType.STRING, value);
+  }
+
+  private isDigit(char: string): boolean {
+    return char >= '0' && char <= '9';
+  }
+
+  // change to be possible to use '.9849' numbers
+  private number(): void {
+    while (this.isDigit(this.peek())) this.advance();
+
+    if (this.peek() === '.' && this.isDigit(this.peek(1))) {
+      this.advance();
+      while (this.isDigit(this.peek()));
+    }
+
+    this.addToken(
+      TokenType.NUMBER,
+      Number(this.source.substring(this.start, this.current)),
+    );
+  }
+
+  private isAlpha(char: string): boolean {
+    return /[a-zA-Z_]/.test(char);
+  }
+
+  private isAlphaNumeric(char: string): boolean {
+    return this.isAlpha(char) || this.isDigit(char);
+  }
+
+  private identifier(): void {
+    while (this.isAlphaNumeric(this.peek())) this.advance();
+
+    const text = this.source.substring(this.start, this.current);
+    const type = keywords.get(text) || TokenType.IDENTIFIER;
+
+    this.addToken(type);
+  }
+
   private scanToken(): void {
     const c = this.advance();
     switch (c) {
@@ -71,8 +142,45 @@ export class Scanner {
       case '*':
         this.addToken(TokenType.STAR);
         break;
+      case '!':
+        this.addToken(this.match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        this.addToken(
+          this.match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL,
+        );
+        break;
+      case '<':
+        this.addToken(this.match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        this.addToken(
+          this.match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER,
+        );
+        break;
+      case '/':
+        if (this.match('/')) {
+          // comment
+          while (this.peek() !== '\n' && !this.isAtEnd()) this.advance();
+        } else {
+          this.addToken(TokenType.SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        this.line++;
+        break;
+      case '"':
+        // strings
+        this.string();
+        break;
       default:
-        LoxEvents.emit('error', `Unexpected character.`);
+        if (this.isDigit(c)) this.number();
+        else if (this.isAlpha(c)) this.identifier();
+        else LoxEvents.emit('error', `Unexpected character.`);
         break;
     }
   }
